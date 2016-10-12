6e34646b7d2ec0469fa3793de606e899525ee8fb
DavidMartinPerez <davidmartinperez1@gmail.com>
2016-10-12 19:18:42 +0200

    ¡Ejercicios Java Actualizados por Capitulos!

---
+++ Capitulo_4/Ejercicio05.java
@@ -0,0 +1,22 @@
+/*
+ * Ejercicio 1 Tema 4 
+ * @uthor David Martin Peréz
+ */
+
+public class Ejercicio05 {
+  
+  public static void main (String args[]) {
+    
+    System.out.print("Por favor, introduzca el valor de a: ");
+    Double a = Double.parseDouble(System.console().readLine());
+    System.out.print("Ahora introduzca el valor de b: ");
+    Double b = Double.parseDouble(System.console().readLine());
+
+    if (a == 0) {
+      System.out.println("Esa ecuación no tiene solución real.");
+    } else {
+      System.out.println("x = " + (-b/a));
+  }
+
+ }
+}
