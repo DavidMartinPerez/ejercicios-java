44c88b151302f91556a71cb055db5fad3d0e162d
DavidMartinPerez <davidmartinperez1@gmail.com>
2016-10-14 19:42:15 +0200

    Ejercicio 8 Java Capitulo 4

---
+++ Capitulo_4/Ejercicio08.java
@@ -0,0 +1,45 @@
+/*
+ * Ejercicio 8 Tema 4 
+ * @uthor David Martin Peréz
+ */
+
+
+
+public class Ejercicio08 {
+  
+  public static void main (String args[]) {
+    System.out.print("Por favor, introduzca la primera nota: ");
+    Double nota1 = Double.parseDouble(System.console().readLine());
+    
+    System.out.print("Ahora introduzca la segunda nota: ");
+    Double nota2 = Double.parseDouble(System.console().readLine());
+    
+    System.out.print("Por último introduzca la tercera nota: ");
+    Double nota3 = Double.parseDouble(System.console().readLine());
+
+    double media = (nota1 + nota2 + nota3) / 3;
+
+    System.out.printf("La media es %.2f\n", media);
+    
+    if (media < 5) {
+      System.out.print("Insuficiente");
+    }
+    
+    if ((media >= 5) && (media < 6)) {
+      System.out.print("Suficiente");
+    }
+    
+    if ((media >= 6) && (media < 7)) {
+      System.out.print("Bien");
+    }
+    
+    if ((media >= 7) && (media < 9)) {
+      System.out.print("Notable");
+    }
+    
+    if (media >= 9) {
+      System.out.print("Sobresaliente");
+    }
+  }
+
+}
