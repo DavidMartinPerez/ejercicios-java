bcb7191d8f9b4e0c34167b599d89ee29ea5ecfe8
DavidMartinPerez <davidmartinperez1@gmail.com>
2016-10-18 17:51:29 +0200

    Java Capitulo4

--- Capitulo_4/Ejercicio09.java
+++ Capitulo_4/Ejercicio09.java
@@ -6,7 +6,54 @@
 public class Ejercicio09 {
   
   public static void main (String args[]) {
+    double x1;
+    double x2;
+
+    System.out.println("Resolvedor de ecuaciones de segundo grado.");
+    //ax^2 + bx + c = 0
     
+    System.out.println("Introduzca los valores.");
+    
+    System.out.print("a = ");
+    double a = Double.parseDouble(System.console().readLine());
+    
+    System.out.print("b = ");
+    double b = Double.parseDouble(System.console().readLine());
+    
+    System.out.print("c = ");
+    double c = Double.parseDouble(System.console().readLine());
+    
+    if ((a == 0) && (b == 0) && (c == 0)) {
+      System.out.println("Tiene infinitas soluciones.");
+    }
+    
+    
+    if ((a == 0) && (b == 0) && (c != 0)) {
+      System.out.println("No tiene solución.");
+    }
+          
+    if ((a != 0) && (b != 0) && (c == 0)) {
+      System.out.println("x1 = 0");
+      System.out.println("x2 = " + (-b / a));
+    }
+    
+    if ((a == 0) && (b != 0) && (c != 0)) {
+      System.out.println("x1 = x2 = " + (-c / b));
+    }
+    
+    if ((a != 0) && (b != 0) && (c != 0)) {  
+        
+      double discriminante = b*b - (4 * a * c);
+      
+      if (discriminante < 0) {
+          System.out.println("No tiene soluciones reales");
+      }  else {
+        System.out.println("x1 = " + (-b + Math.sqrt(discriminante))/(4 * a * c));
+        System.out.println("x2 = " + (-b - Math.sqrt(discriminante))/(4 * a * c));
+      }
+    }
+          
   }
 }
+
 
---
+++ Capitulo_4/Ejercicio10.java
@@ -0,0 +1,108 @@
+/*
+ * Ejercicio 10 Tema 4 
+ * @uthor David Martin Peréz
+ */
+
+public class Ejercicio10 {
+  
+  public static void main (String args[]) {
+        String horoscopo = "";
+
+    System.out.println("Este programa milagroso le adivinara su horoscopo(^^)");
+    System.out.print("Introduce tu mes de nacimiento (1-12): ");
+    int mes = Integer.parseInt(System.console().readLine());
+    
+    System.out.print("Y ahora introduce su dia de nacimiento: ");
+    int dia = Integer.parseInt(System.console().readLine());
+  
+    switch(mes) {
+      case 1:
+        if (dia < 21) {
+          horoscopo = "capricornio";
+        } else {
+          horoscopo = "acuario";
+        }
+        break;
+      case 2:
+        if (dia < 20) {
+          horoscopo = "acuario";
+        } else {
+          horoscopo = "piscis";
+        }
+        break;
+      case 3:
+        if (dia < 21) {
+          horoscopo = "piscis";
+        } else {
+          horoscopo = "aries";
+        }
+        break;
+      case 4:
+        if (dia < 21) {
+          horoscopo = "aries";
+        } else {
+          horoscopo = "tauro";
+        }
+        break;
+      case 5:
+        if (dia < 20) {
+          horoscopo = "tauro";
+        } else {
+          horoscopo = "géminis";
+        }
+        break;
+      case 6:
+        if (dia < 22) {
+          horoscopo = "géminis";
+        } else {
+          horoscopo = "cáncer";
+        }
+        break;
+      case 7:
+        if (dia < 22) {
+          horoscopo = "cáncer";
+        } else {
+          horoscopo = "Leo";
+        }
+        break;
+      case 8:
+        if (dia < 24) {
+          horoscopo = "leo";
+        } else {
+          horoscopo = "virgo";
+        }
+        break;
+      case 9:
+        if (dia < 23) {
+          horoscopo = "virgo";
+        } else {
+          horoscopo = "libra";
+        }
+        break;
+      case 10:
+        if (dia < 23) {
+          horoscopo = "libra";
+        } else {
+          horoscopo = "escorpio";
+        }
+        break;
+      case 11:
+        if (dia < 23) {
+          horoscopo = "escorpio";
+        } else {
+          horoscopo = "sagitario";
+        }
+        break;
+      case 12:
+        if (dia < 21) {
+          horoscopo = "sagitario";
+        } else {
+          horoscopo = "capricornio";
+        }
+        break;
+      default:
+    }
+    System.out.println("Adivinando...");
+    System.out.print("Y su horoscopo es ni mas ni menos que " + horoscopo);
+  }
+}
