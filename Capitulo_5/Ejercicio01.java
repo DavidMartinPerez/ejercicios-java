7ed29cce3c77a1423b384c810159274ede618614
DavidMartinPerez <davidmartinperez1@gmail.com>
2016-10-25 13:16:15 +0200

    Ejercicios Capitulo 5 JAVA

---
+++ Capitulo_5/Ejercicio01.java
@@ -0,0 +1,15 @@
+/*
+ * Ejercicio 1 Tema 4 
+ * @uthor David Martin Peréz
+ */
+
+
+public class Ejercicio01 {
+  
+   public static void main (String args[]) {
+     for (int i = 0; i <= 100; i +=5){
+        System.out.println("La i vale" + i);
+    }
+  }
+}
+     
