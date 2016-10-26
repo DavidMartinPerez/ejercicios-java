41a66c52fd4785143756a4b975b9965f5e098641
DavidMartinPerez <davidmartinperez1@gmail.com>
2016-10-26 16:54:35 +0200

    Java ejercicios capitulo 5

---
+++ Capitulo_5/Ejercicio02.java
@@ -0,0 +1,16 @@
+/*
+ * Ejercicio 2 Capitulo 5 
+ * @uthor David Martin Peréz
+ */
+
+public class Ejercicio02 {
+  
+  public static void main (String args[]) {
+    int i = 0;
+    
+    while(i <= 100) {
+      System.out.println(i);
+      i+=5;
+    }
+  }
+}
