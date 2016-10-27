5285374bc0284d1f0d0a76cfa1c76abee5ac4a06
DavidMartinPerez <davidmartinperez1@gmail.com>
2016-10-27 21:08:21 +0200

    actualizado

---
+++ Capitulo_5/Ejercicio03.java
@@ -0,0 +1,16 @@
+/*
+ * Ejercicio 3 Capitulo 5 
+ * @uthor David Martin Peréz
+ */
+
+public class Ejercicio03 {
+  
+  public static void main (String args[]) {    
+    int i = 0;
+    
+    do {
+      System.out.println(i);
+      i+=5;
+    } while(i <= 100);
+  }
+}
