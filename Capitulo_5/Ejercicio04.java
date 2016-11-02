6a69075c4f7db4c9af27d4ebdd900212a9f870e9
DavidMartinPerez <davidmartinperez1@gmail.com>
2016-11-02 10:08:20 +0100

    ejercicio 5 capitulo 5

---
+++ Capitulo_5/Ejercicio04.java
@@ -0,0 +1,13 @@
+/*
+ * Ejercicio 4 Capitulo 5 
+ * @uthor David Martin Peréz
+ */
+
+public class Ejercicio04 {
+  
+  public static void main (String args[]) { 
+    for(int i = 320; i >= 160; i-=20) {
+      System.out.println(i);
+    }
+  }
+}
---
+++ Capitulo_5/Ejercicio05.java
@@ -0,0 +1,15 @@
+/*
+ * Ejercicio 5 Capitulo 5 
+ * @uthor David Martin Peréz
+ */
+
+public class Ejercicio05 {
+  
+  public static void main (String args[]) { 
+    int i = 320;
+    while(i >= 160) {
+      System.out.println(i);
+      i-=20;
+    }
+  }
+}
