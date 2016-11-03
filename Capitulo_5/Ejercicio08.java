f10381a1a9843ab0788827346e02e35aacaf5c78
DavidMartinPerez <davidmartinperez1@gmail.com>
2016-11-03 13:31:55 +0100

    Ejercicio 8 Java Bucles

---
+++ Capitulo_5/Ejercicio08.java
@@ -0,0 +1,22 @@
+/*
+ * Ejercicio 8 Capitulo 5 
+ * @uthor David Martin Peréz
+ */
+
+public class Ejercicio08 {
+  
+  public static void main (String args[]) { 
+    System.out.println("__La Multiplicadora__");
+    System.out.println("-----------------------");
+    System.out.print("Introduce un número: ");
+    int numero = Integer.parseInt(System.console().readLine());
+    
+    int i = 0;
+    
+    while (i < 10) {
+    System.out.println(numero + " * " + i + " = " + numero * i);
+    i++;
+  } 
+ }
+}
+
