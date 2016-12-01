e291f2b6a725fff733022d18f5e897f0aaffa877
DavidMartinPerez <davidmartinperez1@gmail.com>
2016-12-01 12:33:53 +0100

    Ejercicios Arrays

---
+++ Capitulo_7/Ejercicio01.java
@@ -0,0 +1,23 @@
+/**
+ * Ejercicio 1 del Capitulo 7 Arrays
+ * Basico Arrays
+* @uthor David Martín Pérez
+ */
+
+public class Ejercicio01 {
+  public static void main(String[] args) {
+    int[] num = new int[12];
+      num[0] = 39;
+      num[1] = -2;
+      num[4] = 0;
+      num[6] = 14;
+      num[8] = 5;
+      num[9]= 120;
+        
+      for (int i=0;i < 12;i++){
+      System.out.println("num[" + i +"]: " + num[i]);
+    }
+    System.out.println();
+    System.out.println("Se inicializan en 0 por defecto");
+  }
+}
