42230b51b2949a236c70f739dd17bf809b0d9824
DavidMartinPerez <davidmartinperez1@gmail.com>
2016-12-09 13:41:43 +0100

    Actaulizando

---
+++ Capitulo_6/Ejercicio13.java
@@ -0,0 +1,20 @@
+/**
+ * Ejercicio 13 del Capitulo 6
+ * Tirada de dados ⚀ ⚁ ⚂ ⚃ ⚄ ⚅
+* @uthor David Martín Pérez
+ */
+
+public class Ejercicio13 {
+
+  public static void main(String[] args) {
+  
+
+  for(int i=0;dado1 == dado2;i++){
+      int dado1 = (int)((Math.random() * 6)+1);
+      int dado2 = (int)((Math.random() * 6)+1);
+    switch(dado1){
+      case 1: 
+        System.out.println("⚀");
+  }
+}
+
