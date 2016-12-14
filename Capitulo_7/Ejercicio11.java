d40f4cc4d202b6125bb2bc7e4c61b6fe166ff3c3
DavidMartinPerez <davidmartinperez1@gmail.com>
2016-12-14 16:58:04 +0100

    Actualizad

--- Capitulo_7/Ejercicio11.java
+++ Capitulo_7/Ejercicio11.java
@@ -7,29 +7,91 @@
 public class Ejercicio11 {
 
   public static void main(String[] args) {
+    //Declarar arrays
     int[] num = new int[10];
+    int[] noPrimo = new int[10];
+    int[] primo = new int[10];
+    //declaras variables
     int n=0;
+    int primos=0;
+    int noPrimos=0;
+    int i=0;
+    int j=0;
+    boolean esPrimo = false;
+    
+    //Pedir el array y mostrarlo
     System.out.print("Introduzce 10 numeros(Press Enter): ");
-    for(int i=0; i < 10; i++){
+    
+    for(i=0; i < 10; i++){
       num[i]= Integer.parseInt(System.console().readLine());
+      
+      esPrimo = true;
+      for (j = 2; j < num[i] - 1; j++) {
+        if (num[i] % j == 0) {
+          esPrimo = false;
+        }
+      }
+    
+
+        if(esPrimo){
+          primo[primos++]= num[i];
+        }else {
+          noPrimo[noPrimos++] = num[i]; 
+        }
     }
+    //Imprimir el array original
     System.out.println("Array Original");
-    System.out.println("\n       ╔═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╗");
     System.out.print("Indice ");
-    for (int i = 1;i < 11;i++) {
+    for (i = 1;i < 11;i++) {
       int m = i;
-      System.out.printf("║%3d  ", m);
+      System.out.printf("%4d  ", m);
+    }
+    System.out.println();
+     System.out.println("       ╔═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╗");
+    System.out.print("Valor  ");
+    for (i=0;i < 10;i++) {
+      int z=num[i];
+      System.out.printf("║%3d  ", z);
     }
     System.out.println("║\n       ╚═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╝");
-    System.out.println("       ╔═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╗");
+    
+    
+    //Ordenar el array
+    for(i=0;i<primos;i++){
+      num[i]=primo[i];
+    }
+    
+    for(i = primos; i < primos + noPrimos; i++){
+      num[i] =noPrimo[i - primos];
+    }
+    
+    
+    
+    //Imprimir el otro array
+    System.out.println("Array modificado");
+    System.out.println("--------------------");
+    System.out.print("Indice ");
+    for (i = 1;i < 11;i++) {
+      int m = i;
+      System.out.printf("%4d  ", m);
+    }
+    System.out.println();
+     System.out.println("       ╔═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╗");
     System.out.print("Valor  ");
-    for (int i=0;i < 10;i++) {
+    for (i=0;i < 10;i++) {
       n=num[i];
       System.out.printf("║%3d  ", n);
     }
     System.out.println("║\n       ╚═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╝");
-  }
-}
+
+        
+      }
+    }
+
+
+
+
+
 
 
   
