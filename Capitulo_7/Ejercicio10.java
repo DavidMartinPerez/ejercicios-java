343daf4497ca5c44498b4d520cda3740d1fec8a5
DavidMartinPerez <davidmartinperez1@gmail.com>
2016-12-14 10:10:56 +0100

    Actualizado

--- Capitulo_7/Ejercicio10.java
+++ Capitulo_7/Ejercicio10.java
@@ -9,14 +9,32 @@ public class Ejercicio10 {
   public static void main(String[] args) {
     
     int[] num = new int[20];
-    for(int i=0;i < 20;i++){
+    int[] impar = new int [20];
+    int[] par = new int[20];
+    
+    int pares = 0;
+    int impares = 0; 
+    
+    for(int i=0;i < 20;i++){//un for para poner en un array los pares y en otro los impares
       num[i] = (int)((Math.random() * 101));
+      if(num[i] % 2 == 0){
+        par[pares++]= num[i];
+      }else {
+        impar[impares++] = num[i]; 
+      }
     }
-    
-    for(int i=0;i < 20;i++){
-      System.out.print(num[i]+" | ");
-    }
-    
-    
+     System.out.println("----");
+    for(int i=0;i<20;i++){ //for para escribir 
+     
+      
+      for(int j=0; j < 20;j++){ //aqui escribe los pares al principio
+        num[j] = par[j];
+      }
+      
+      for(int k=pares; k<20;k++){ //aqui escribe los impares al final del array por que empieza desde los pares
+        num[k] = impar[k - pares];
+      }
+      System.out.println(num[i]);
   }
+}
 }
