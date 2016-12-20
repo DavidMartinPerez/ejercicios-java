7fb75d8df778a6a3831c52fdc2a5d7765b7ddbe4
DavidMartinPerez <davidmartinperez1@gmail.com>
2016-12-20 10:13:20 +0100

    Actualizando

---
+++ Capitulo_7.2/Ejercicio05.java
@@ -0,0 +1,21 @@
+/**
+* Ejercicios Capitulo 7 Array Bidimensionales
+* Ejercicio 5 
+* 
+* @uthor David Martín Pérez 
+ */
+public class Ejercicio04 {
+  public static void main(String[] args) throws InterruptedException {
+    int[][] num = new int[6][10];
+    int fila;
+    int columna;
+    
+    for(fila = 0; i < 6;i++){
+      
+    }
+    
+    
+    
+  }
+}
+
